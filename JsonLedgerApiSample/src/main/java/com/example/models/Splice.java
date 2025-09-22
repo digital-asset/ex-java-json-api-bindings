9592bb475e3cd46f8ecd85db7b1b709712f5186f
Andre Marques Lee <andre.marqueslee@digitalasset.com>
2025-09-22 20:49:01 +1000

    Remove SampleUser

--- JsonLedgerApiSample/src/main/java/com/example/models/Splice.java
+++ JsonLedgerApiSample/src/main/java/com/example/models/Splice.java
@@ -1,20 +1,7 @@
 package com.example.models;
 
-import com.example.SampleUser;
-import splice.api.token.holdingv1.Holding;
-import splice.api.token.holdingv1.HoldingView;
-import splice.api.token.holdingv1.InstrumentId;
-import splice.api.token.metadatav1.ChoiceContext;
-import splice.api.token.metadatav1.ExtraArgs;
-import splice.api.token.metadatav1.Metadata;
-import splice.api.token.transferinstructionv1.Transfer;
-import splice.api.token.transferinstructionv1.TransferFactory_Transfer;
 import splice.wallet.transferpreapproval.TransferPreapprovalProposal;
 
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 public class Splice {
